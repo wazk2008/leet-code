e30a1d7d689e8d80cc3637a4bd286d05704f56b7
zk <15010102217@163.com>
2021-03-03 23:48:28 +0800

    完成【338-比特位计数】

---
+++ src/main/java/com/leetcode/code/Code338.java
@@ -0,0 +1,33 @@
+package com.leetcode.code;
+
+import sun.tools.java.BinaryCode;
+
+/**
+ * @Description:
+ * @ClassName: Code338
+ * @Author: ZK
+ * @Date: 2021/3/3 23:14
+ * @Version: 1.0
+ */
+public class Code338 {
+
+    public static void main(String[] args) {
+        int x = 11;
+        int y = 12;
+        System.out.println(Integer.toBinaryString(x));
+        System.out.println(Integer.toBinaryString(y));
+        System.out.println(Integer.toBinaryString(x & y));
+        System.out.println(x & y);
+    }
+
+    public static int[] countBits(int num) {
+        int[] res = new int[num+1];
+
+        for (int i = 1; i <= num; i++) {
+            res[i] = res[ i&(i-1) ] + 1;
+        }
+
+        return res;
+    }
+
+}

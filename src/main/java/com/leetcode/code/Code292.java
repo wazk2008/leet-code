5cf61bb7d475dbd00e1c69a93fb501b11dc252e1
zhangkun <15010102217@163.com>
2021-09-18 00:08:24 +0800

    完成【292-Nim 游戏】

---
+++ src/main/java/com/leetcode/code/Code292.java
@@ -0,0 +1,13 @@
+package com.leetcode.code;
+
+public class Code292 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public boolean canWinNim(int n) {
+        return n%4!=0;
+    }
+
+}
